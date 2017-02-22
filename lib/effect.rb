1f78fee80f2b8d704f5ee687d568e2eaa348ea14
lister <simon@lisuna.org>
2017-02-22 20:53:17 +0100

    Add fire.

--- lib/effect.rb
+++ lib/effect.rb
@@ -2,6 +2,7 @@ require_relative 'effect/base'
 require_relative 'effect/off'
 require_relative 'effect/random_color'
 require_relative 'effect/progress_bar'
+require_relative 'effect/fire'
 
 module Effect
 
